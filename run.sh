8f20ec442cd5fd0be9cab26d4c8b15bd43684a31
Eugene Kim <genakim@gl-login3.arc-ts.umich.edu>
2023-12-01 16:30:06 -0500

    script

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@
 #SBATCH --job-name causalBERT
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=10g
+#SBATCH --mem-per-cpu=5g
 #SBATCH --time=00:15:00
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=1
 #SBATCH --mail-type=begin,end
 #SBATCH --mail-user=genakim@umich.edu
+#SBATCH -o ./slurm-outputs/%j.out
 python CausalBert.py > slurm-outputs/output-$SLURM_JOBID.txt
