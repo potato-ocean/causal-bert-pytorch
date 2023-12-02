57563edd0d83ae6d2aa786779a563c84f79f22f1
Eugene Kim <genakim@gl-login3.arc-ts.umich.edu>
2023-12-02 16:19:42 -0500

    hello

--- CausalBertInference.py
+++ CausalBertInference.py
@@ -2,7 +2,7 @@ from CausalBert import CausalBertWrapper
 import torch
 
 
-cb = torch.load(CausalBertWrapper, ',/causal-bert-peer-read')
+cb = torch.load(CausalBertWrapper, './causal-bert-peer-read')
 ATE = cb.ATE(df['C'], df.text, platt_scaling=True)
 Q_ATT = cb.Q_ATT(df['C'], df['text'], df['T'])
 plug_in_ATT = cb.plug_in_ATT(df['C'], df['text'], df['T'], cb.loss_weights['g'])
