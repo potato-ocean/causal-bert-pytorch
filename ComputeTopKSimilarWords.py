a9c4603b139ccbf78652b4247a7679b5cc5d06eb
Nicholas David <nrdavid74@gmail.com>
2023-12-04 16:27:23 -0500

    clean up topK code

--- ComputeTopKSimilarWords.py
+++ ComputeTopKSimilarWords.py
@@ -1,4 +1,3 @@
-from scipy.spatial.distance import cosine
 from transformers import BertTokenizer, BertModel
 import torch
 import pandas as pd
@@ -37,11 +36,27 @@ tokenizer = BertTokenizer.from_pretrained('distilbert-base-uncased') #replace wi
 model = BertModel.from_pretrained('fine-tuned-causal-bert', output_hidden_states=True)
     
 def tokenize(text):
+    '''
+    Tokenize a given piece of text. Helper function for get_embedding.
+    Args:
+        text (str): Some text
+    Returns:
+        tokens (list): Tokenized text by BERT
+    '''
     marked_text = "[CLS] " + text + " [SEP]"
     tokens = tokenizer.tokenize(marked_text)
     return tokens
 
 def get_embedding(text):
+    '''
+    Computes the embedding for a piece of text.
+    Args:
+        text (str): Some text
+    Returns:
+        tokens (list): The tokens generated from the tokenizer
+        token_vecs (list): The embedding of the token
+        abstract_embedding (torch.Tensor): The abstract level embedding
+    '''
     tokens = tokenize(text)
     indexed_tokens = tokenizer.convert_tokens_to_ids(tokens)
     segments_ids = [1] * len(indexed_tokens)
@@ -56,47 +71,34 @@ def get_embedding(text):
 
     token_embeddings = torch.stack(hidden_states, dim=0)
     token_embeddings = torch.squeeze(token_embeddings, dim=1)
-    # Swap dimensions 0 and 1.
     token_embeddings = token_embeddings.permute(1,0,2)
+    # Now N x 12 x 768
     token_vecs_cat = []
 
     for token in token_embeddings:
-        # `token` is a [12 x 768] tensor
-
-        # Concatenate the vectors (that is, append them together) from the last 
-        # four layers.
-        # Each layer vector is 768 values, so `cat_vec` is length 3,072.
+        # concatenate last 4 layers since shows best performance
+        # See https://mccormickml.com/2019/05/14/BERT-word-embeddings-tutorial/
         cat_vec = torch.cat((token[-1], token[-2], token[-3], token[-4]), dim=0)
-        # Use `cat_vec` to represent `token`.
         token_vecs_cat.append(cat_vec)
-    # Stores the token vectors, with shape [22 x 768]
-    
     token_vecs_sum = []
-
-    # `token_embeddings` is a [22 x 12 x 768] tensor.
-
-    # For each token in the sentence...
     for token in token_embeddings:
 
-        # Sum the vectors from the last four layers.
+        # now sum the last 4 layers
         sum_vec = torch.sum(token[-4:], dim=0)
-        
-        # Use `sum_vec` to represent `token`.
         token_vecs_sum.append(sum_vec)
-    # For sentence embeds
+
+    # Compute the embeddings for the entire abstract
     token_vecs = hidden_states[-2][0]
-    sentence_embedding = torch.mean(token_vecs, dim=0)
-    # token_embeddings = hidden_states[-2][0]
-    # sentence_embedding = torch.mean(token_embeddings, dim=0)
-    return tokens, token_vecs, sentence_embedding
+    abstract_embedding = torch.mean(token_vecs, dim=0)
+    return tokens, token_vecs, abstract_embedding
 
 original_buzzy_embeddings = {}
+# must parse some buzzy words different based off BERT tokenizer
 HOLD_AD = []
 HOLD_EMB = []
 for part in contextualized_buzzy:
     e = get_embedding(part)
     for token, token_vec in zip(e[0], e[1]):
-        #logger.info(f'{token}')
         if token in ["ad", "##vers", "##aria", "##l]"]:
             HOLD_AD.append(token_vec)
         if token in ["em", "##bed", "##ding", "##s"]:
@@ -104,52 +106,48 @@ for part in contextualized_buzzy:
         if token in ['deep', 'embedding', 'adversarial', 'neural']:
             original_buzzy_embeddings[token] = get_embedding(token)[1][1, :] # rid of [cls] and [sep]
 
+# Stack and sum the sub-word token embeddings
 original_buzzy_embeddings["adversarial"] = torch.stack(HOLD_AD, dim=0).sum(dim=0).squeeze()
 original_buzzy_embeddings["embed"] = torch.stack(HOLD_EMB, dim=0).sum(dim=0).squeeze()
 
-# logger.info(f'{original_buzzy_embeddings["adversarial"]}')
-
-df = pd.read_csv("PeerRead_with_abstract.csv")
+# read the abstracts
 def sim(abstract, original_buzzy_embeddings, cos, threshold=0.3):
+    '''
+    Args:
+        abstract (string): The abstract
+        original_buzzy_embeddings: A dictionary of the original buzzy embeddings
+                                    and their embeddings
+        cos (torch.nn): Pytorch cosine similarity function
+        threshold (float): Value to threshold similarity scores by for efficiency
+
+    Returns:
+        similar_word (dictionary): A dictionary of similar words with keys = tokens
+                                    and values = similarity score
+    '''
     similar_words = {}
     tokens, token_vecs, _ = get_embedding(abstract)
     for token, word_embed in zip(tokens, token_vecs):
         for word, emb in original_buzzy_embeddings.items():
-            # logger.info(f"{word_embed}")
-            # logger.info(f"{emb[1, :]}")
-            # logger.info(f'{word_embed.shape}')
-            # logger.info(f'{emb.shape}')
+            # Compare the similarity between each word and the words in the
+            # buzzy set
+            # 1 cos means values close to 0 are similar.
             similarity = 1 - cos(word_embed, emb)
-            # logger.info(f'{word}, {token}')
-            # logger.info(f'{similarity}')
-            if similarity > threshold: #and word not in original_buzzy:
+            if similarity < threshold:
                 similar_words[token] = similarity
     return similar_words
 
+df = pd.read_csv("PeerRead_with_abstract.csv")
 cos = torch.nn.CosineSimilarity(dim=0)
-
 logger.info("start top k")
-L = df['abstract'].apply(lambda x: sim(x, original_buzzy_embeddings, cos))
+# Apply to all abstracts in dataset
+L = df.head['abstract'].apply(lambda x: sim(x, original_buzzy_embeddings, cos))
 similar_words = {}
+# update the dictionary with our list of similar word dictionaries
 for d in L:
     similar_words.update(d)
 logger.info("stop top k")
-file_path = "similar_words.txt"
+file_path = "similar_words_new.txt"
+# write the words and similarity scores to file
 with open(file_path, 'w') as file:
     for k, v in similar_words.items():
         file.write(f'{k} : {v}' + '\n')
-# logger.info("start top k")
-# for i, chunk in enumerate(np.array_split(df, 120)):
-#     similar_words = {}
-#     logger.info(f'{chunk["abstract"]}')
-#     L = chunk['abstract'].apply(lambda x: sim(x, original_buzzy_embeddings, cos))
-#     logger.info(f'{L}')
-#     for d in L:
-#         similar_words.update(d)
-#     logger.info(f"Done top k for chunk {i}")
-#     file_path = "similar_words.txt"
-#     with open(file_path, 'a') as file:
-#         for k, v in similar_words.items():
-#             logger.info(f'{k}')
-#             file.write(f'{k} : {v}' + '\n')
-#     logger.info(f"Wrote to file for chunk {i}")
